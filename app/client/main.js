a3af53215127ed860dcb5880430a0245e6393f11
Jonathan M Nguyen <31453488+jonathanMNg@users.noreply.github.com>
2017-10-12 10:22:18 -1000

    WOD: Meteor-Semantic

---
+++ app/client/main.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+  thatsAlot() {
+    if(Template.instance().counter.get() > 5)
+      return "That’s a lot!";
+  },
+});
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
